6643115b8bf28d88196ee1e455288feb9e7e3173
tedi3231 <tedi3231@gmail.com>
2013-07-28 22:31:07 +0800

    now we can set oanum for feebase and feeforsend

--- tms/wizard/tms_wizard_oa.py
+++ tms/wizard/tms_wizard_oa.py
@@ -8,29 +8,33 @@ class tms_wizard_oa(osv.osv_memory):
     _columns = {
         "feebase_ids":fields.one2many("tms.wizard.item", "wizard_id", string="Wizard"),
         'oanum': fields.char(string='OANum',size=50, required=True),
+        'feetype':fields.char(string="Feetype",size=100,)
     }
     
     def default_get(self, cr, uid, fields, context=None):
         if context == None:
             context = {}            
         wizard_id = context.get('acvite_id', None)
+        active_model = context.get("active_model",None)
+        if not active_model:
+            return False
         res = []
         product_ids = context.get('active_ids', [])
-        products = self.pool.get('tms.feebase').browse(cr, uid, product_ids, context=context)
+        products = self.pool.get(active_model).browse(cr, uid, product_ids, context=context)
         for pitem in products:
             res.append({
                 'wizard_id':wizard_id,
-                'feebase_id':pitem.id
+                'feebase_id':pitem.id,
+                'feetype':active_model,
             })
-        return {'feebase_ids':res}
+        return {'feebase_ids':res,'feetype':active_model}
     
     def setoanum(self, cr, uid, ids,context=None):
+        print context
+        active_model = context.get("active_model",None)
         wizard = self.browse(cr,uid,ids,context)[0]
-        #print "setoanum() params ids is %s,context is %s"%(ids,context)
-        print 'wizard is %s '%wizard
         fees = wizard.feebase_ids
-        print 'fees list is %s'%fees
-        feebase=self.pool.get("tms.feebase")
+        feebase=self.pool.get(active_model)
         for item in fees:
             feebase.write(cr,uid,item.feebase_id.id,{"state":"hasoa","oanum":wizard.oanum})            
         return True
@@ -46,3 +50,23 @@ class tms_wizard_item(osv.osv_memory):
         "feebase_id":fields.many2one('tms.feebase', string="Fee", required=True),
     }
 tms_wizard_item()    
+
+
+class tms_wizard_oa_forsend(osv.osv_memory):
+    _name = "tms.wizard.oa.forsend"
+    _inherit = "tms.wizard.oa"
+    _description = "Set oa"    
+
+    _columns = {
+        "feebase_ids":fields.one2many("tms.wizard.item.forsend", "wizard_id", string="Wizard"),
+    }
+tms_wizard_oa_forsend()
+
+class tms_wizard_item_forsend(osv.osv_memory):
+    _name = "tms.wizard.item.forsend"
+    _description = "FeeBase Item"
+    _columns = {
+        "wizard_id":fields.many2one('tms.wizard.oa.forsend', string="Wizard", required=True),
+        "feebase_id":fields.many2one('tms.feeforsend', string="Fee", required=True),
+    }
+tms_wizard_item_forsend()    
